c70239456528c7b2a02a14b4700fa0fb0727aa14
TendTo <casablancaernesto@gmail.com>
2022-10-22 11:14:53 +0200

    test(tests): add many base circuit tests

--- tests/unit/test_circuit.py
+++ tests/unit/test_circuit.py
@@ -1,8 +1,13 @@
-# pylint: disable=missing-function-docstring,too-few-public-methods
+# pylint: disable=missing-function-docstring,too-few-public-methods,redefined-outer-name
 import pytest
 import numpy as np
 from qclight.error import BinaryStringError, PowerOfTwoLengthError, PositiveValueError
-from qclight.circuit import QCLCircuit
+from qclight.circuit import QCLCircuit, SumCircuit
+
+
+@pytest.fixture(scope="function")
+def circuit():
+    return QCLCircuit(3)
 
 
 class TestCircuit:
@@ -54,5 +59,92 @@ class TestCircuit:
             with pytest.raises(PositiveValueError, match=r"received '0'"):
                 QCLCircuit(0)
 
+        def test_tp(self, circuit: QCLCircuit):
+            gates = [np.array([[0, 1], [1, 0]])] * 2
+            tp_gates = circuit.tp(gates)
+            assert tp_gates.shape == (4, 4)
+            assert np.array_equal(
+                tp_gates,
+                np.array([[0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0]]),
+            )
+
+        def test_x_gate_int(self, circuit: QCLCircuit):
+            circuit.x(1)
+            assert np.array_equal(circuit.run(), [0, 0, 1, 0, 0, 0, 0, 0])
+
+        def test_x_gate_list(self, circuit: QCLCircuit):
+            circuit.x([0, 2])
+            assert np.array_equal(circuit.run(), [0, 0, 0, 0, 0, 1, 0, 0])
+
+        def test_h_gate_int(self, circuit: QCLCircuit):
+            circuit.h(1)
+            assert np.allclose(
+                circuit.run(), [0.70710678, 0, 0.70710678, 0, 0, 0, 0, 0]
+            )
+
+        def test_h_gate_list(self, circuit: QCLCircuit):
+            circuit.h([0, 2])
+            assert np.allclose(circuit.run(), [0.5, 0.5, 0, 0, 0.5, 0.5, 0, 0])
+
+        def test_cx_gate_c_zero(self, circuit: QCLCircuit):
+            circuit.cx(0, 2)
+            assert np.allclose(circuit.run(), [1, 0, 0, 0, 0, 0, 0, 0])
+
+        def test_cx_gate_c_one(self, circuit: QCLCircuit):
+            circuit.x(0)
+            circuit.cx(0, 1)
+            assert np.allclose(circuit.run(), [0, 0, 0, 0, 0, 0, 1, 0])
+
+        def test_ccx_gate_c_zero(self, circuit: QCLCircuit):
+            circuit.ccx(0, 1, 2)
+            assert np.allclose(circuit.run(), [1, 0, 0, 0, 0, 0, 0, 0])
+
+        def test_ccx_gate_c1_one(self, circuit: QCLCircuit):
+            circuit.x(0)
+            circuit.ccx(0, 1, 2)
+            assert np.allclose(circuit.run(), [0, 0, 0, 0, 1, 0, 0, 0])
+
+        def test_ccx_gate_c2_one(self, circuit: QCLCircuit):
+            circuit.x(1)
+            circuit.ccx(0, 1, 2)
+            assert np.allclose(circuit.run(), [0, 0, 1, 0, 0, 0, 0, 0])
+
+        def test_ccx_gate_c1_c2_one(self, circuit: QCLCircuit):
+            circuit.x(0)
+            circuit.x(1)
+            circuit.ccx(0, 1, 2)
+            assert np.allclose(circuit.run(), [0, 0, 0, 0, 0, 0, 0, 1])
+
+        def test_swap_gate_equal(self, circuit: QCLCircuit):
+            circuit.swap(0, 1)
+            assert np.allclose(circuit.run(), [1, 0, 0, 0, 0, 0, 0, 0])
+
+        def test_swap_gate_different(self, circuit: QCLCircuit):
+            circuit.x(0)
+            circuit.swap(0, 2)
+            assert np.allclose(circuit.run(), [0, 1, 0, 0, 0, 0, 0, 0])
+
     class TestSumCircuit:
-        pass
+        def test_constructor_same_length(self):
+            a, b = 0b101, 0b110
+            sum_c = SumCircuit(a, b)
+            assert sum_c.n == 3 + 3 + 4
+            assert sum_c.sum() == a + b
+
+        def test_constructor_different_length(self):
+            a, b = 0b101, 0b1101
+            sum_c = SumCircuit(a, b)
+            assert sum_c.n == 3 + 4 + 5
+            assert sum_c.sum() == a + b
+
+        def test_constructor_with_zero(self):
+            a, b = 0b0, 0b1111
+            sum_c = SumCircuit(a, b)
+            assert sum_c.n == 1 + 4 + 5
+            assert sum_c.sum() == a + b
+
+        def test_constructor_five_per_five_sums(self):
+            for a in range(10):
+                for b in range(10):
+                    sum_c = SumCircuit(a, b)
+                    assert sum_c.sum() == a + b
