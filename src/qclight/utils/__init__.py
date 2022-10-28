d6b8001a3e3caf5ab0a3df569156677629c752a3
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-10-28 10:47:59 +0200

    fix(utils): fix bit extraction function bitwise operations

--- src/qclight/utils/__init__.py
+++ src/qclight/utils/__init__.py
@@ -4,8 +4,8 @@ These are mostly math and bit manipulation functions.
 
 
 import re
-
-from qclight.error import BinaryStringError
+from typing import Iterator
+from qclight.error import BinaryStringError, OutOfRangeError
 
 
 def bitstring_to_int(
@@ -32,16 +32,9 @@ def extract_bits(number: "int", idx: "int | list[int]") -> "int":
     """Extracts the bits with index idx (or in the list idx)
     from the binary representation of number.
     The bits are then used to build  a new number,
-    in the order they appear in the list, little endian.
+    in the order they appear in the list, big endian.
 
     Example:
-
-    ```python
-    extract_bits(0b1011, 0) # 1
-    extract_bits(0b1011, 1) # 0
-    extract_bits(0b1011, [1, 2, 3]) # 0b011 -> 3
-    extract_bits(0b1011, [3, 1, 1]) # 0b100 -> 4
-    ```
 
         .. code-block:: python
 
@@ -50,19 +43,26 @@ def extract_bits(number: "int", idx: "int | list[int]") -> "int":
             extract_bits(0b1011, [1, 2, 3]) # 0b011 -> 3
             extract_bits(0b1011, [3, 1, 1]) # 0b100 -> 4
 
+    ```python
+    extract_bits(0b1011, 0) # 1
+    extract_bits(0b1011, 1) # 0
+    extract_bits(0b1011, [1, 2, 3]) # 0b011 -> 3
+    extract_bits(0b1011, [3, 1, 1]) # 0b100 -> 4
+    ```
+
     Args:
         number: number in binary where the bits are extracted from
-        idx: index or list of indexes of the bits to extract, little endian
+        idx: index or list of indexes of the bits to extract, big endian
 
     Returns:
         a number build using the extracted bits
     """
     n = number.bit_length()
     if isinstance(idx, int):
-        return number & (1 << (n - idx - 1))
+        return number >> (n - idx - 1) & 1
 
     bits = 0
     for i in idx:
-        bit = (number >> i) & 1
+        bit = (number >> (n - i - 1)) & 1
         bits = (bits << 1) | bit
     return bits
