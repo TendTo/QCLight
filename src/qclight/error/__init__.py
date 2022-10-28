081255dbb1b642f5ab37b7737fa7b130c2e9f164
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-10-28 10:48:53 +0200

    feat(error): add OutOfRangeError

--- src/qclight/error/__init__.py
+++ src/qclight/error/__init__.py
@@ -10,21 +10,21 @@ a number of custom exceptions that try to be as explicit as possible.
 class BinaryStringError(ValueError):
     """Raised when the string received is not the string representation of a number in binary."""
 
-    def __init__(self, invalid_string: str) -> None:
+    def __init__(self, invalid_string: "str") -> "None":
         super().__init__(f"Expected a binary string, received '{invalid_string}'")
 
 
 class PowerOfTwoError(ValueError):
     """Raised when the number received is not a power of 2."""
 
-    def __init__(self, invalid_number: int) -> None:
+    def __init__(self, invalid_number: "int") -> "None":
         super().__init__(f"Expected a power of two, received '{invalid_number}'")
 
 
 class PowerOfTwoLengthError(ValueError):
     """Raised when the length of the vector received is not a power of 2."""
 
-    def __init__(self, invalid_number: int) -> None:
+    def __init__(self, invalid_number: "int") -> "None":
         super().__init__(
             f"Expected a vector with a length of a power of two, received '{invalid_number}'"
         )
@@ -35,7 +35,7 @@ class PositiveValueError(ValueError):
     It means it is less or equal to 0.
     """
 
-    def __init__(self, invalid_number: int) -> None:
+    def __init__(self, invalid_number: "int") -> "None":
         super().__init__(f"Expected a positive value, received '{invalid_number}'")
 
 
@@ -44,5 +44,29 @@ class BitValueError(ValueError):
     The value expected was either 0 or 1.
     """
 
-    def __init__(self, invalid_number: int) -> None:
+    def __init__(self, invalid_number: "int") -> "None":
         super().__init__(f"Expected a '0' or '1' value, received '{invalid_number}'")
+
+
+class OutOfRangeError(IndexError):
+    """Raised when the index received is out of range."""
+
+    def __init__(
+        self,
+        max_range: "int",
+        min_range: "int |None" = None,
+        invalid_index: "int | None" = None,
+    ) -> "None":
+        if invalid_index is None and min_range is None:
+            super().__init__(f"Expected an index in the range [0, {max_range}]")
+        elif min_range is None:
+            super().__init__(
+                f"Expected index in the range [0, {max_range}], received '{invalid_index}'"
+            )
+        elif invalid_index is None:
+            super().__init__(f"Expected index in the range [{min_range}, {max_range}]")
+        else:
+            super().__init__(
+                f"Expected index in the range [{min_range}, {max_range}], "
+                f"received '{invalid_index}'"
+            )
